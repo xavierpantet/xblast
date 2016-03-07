6e51c478b3f3adf0c83ad13b9bbfd1bf7c88027e
Timothée Duran <timothee.duran@epfl.ch>
2016-03-07 15:01:45 +0100

    Avnce sur DirectedPosition

--- src/ch/epfl/xblast/server/LifeState.java
+++ src/ch/epfl/xblast/server/LifeState.java
@@ -5,10 +5,22 @@ public final class LifeState {
     private final int lives;
     private final State state;
     
+    /**
+     * Enum qui decrit les trois etats possibles d'un joueur
+     * @author timotheedu
+     *
+     */
     public enum State {
         INVULNERABLE, VULNERABLE, DYING, DEAD;
     }
     
+    /**
+     * Constructeur de la classe
+     * @param lives
+     * @param state
+     * @throws IllegalArgumentException
+     * @throws NullPointerException
+     */
     public LifeState(int lives, State state) throws IllegalArgumentException, NullPointerException{
         
         if(lives<0){
@@ -22,14 +34,26 @@ public final class LifeState {
         
     }
     
+    /**
+     * Methode qui retorune le nombre de vie restant
+     * @return this.lives
+     */
     public int lives (){
        return this.lives;
     }
     
+    /**
+     * Méthode qui retourne l'état actuel d'un joueur
+     * @return this.state
+     */
     public State state(){
         return this.state;
     }
     
+    /**
+     * Methode qui retour true si je jour peut bouger et false sinon
+     * @return
+     */
     public boolean canMove(){
         if ((this.state==State.VULNERABLE)||(this.state==State.INVULNERABLE)){
             return true;
