f8c51ee245ebc8b6e91144a51652c850c4f01109
Timothée Duran <timothee.duran@epfl.ch>
2016-03-11 14:53:29 +0100

    Tests constructeur ok

--- XBlastTest/ch/epfl/xblast/server/PlayerTest.java
+++ XBlastTest/ch/epfl/xblast/server/PlayerTest.java
@@ -2,19 +2,44 @@ package ch.epfl.xblast.server;
 
 import org.junit.Test;
 
+
 import ch.epfl.cs108.Sq;
+import ch.epfl.xblast.Direction;
+import ch.epfl.xblast.SubCell;
 import ch.epfl.xblast.PlayerID;
 import ch.epfl.xblast.server.Player.DirectedPosition;
 import ch.epfl.xblast.server.Player.LifeState;
+import ch.epfl.xblast.server.Player.LifeState.State;
 
 public class PlayerTest {
 
     @Test
-    public void returnErrorWhenConstruction (){
-        Sq<LifeState> l = Sq.constant()
-        
-        Player p = new Player(PlayerID.PLAYER_1, );
-        
-        PlayerID id, Sq<LifeState> lifeStates, Sq<DirectedPosition> directedPos, int maxBombs, int bombRange
+    public void constrctorOK(){
+        Player p = new Player(PlayerID.PLAYER_1, Sq.constant(new Player.LifeState(3, State.INVULNERABLE)), Sq.constant(new Player.DirectedPosition(new SubCell(3,4), Direction.N)), 3, 3);
     }
+    
+    @Test (expected=IllegalArgumentException.class)
+    public void consrtuctionWithNegativeBombNumberReturnError(){
+        Player p = new Player(PlayerID.PLAYER_1, Sq.constant(new Player.LifeState(0, State.INVULNERABLE)), Sq.constant(new Player.DirectedPosition(new SubCell(0,0), Direction.N)), -1, 2);
+    }
+
+    @Test (expected=IllegalArgumentException.class)
+    public void consrtuctionWithNegativeBombSpreadReturnError(){
+        Player p = new Player(PlayerID.PLAYER_1, Sq.constant(new Player.LifeState(0, State.INVULNERABLE)), Sq.constant(new Player.DirectedPosition(new SubCell(0,0), Direction.N)), 1, -2);
+    }
+    @Test (expected=NullPointerException.class)
+    public void consrtuctionWithnullDirectedPosReturnError(){
+        Player p = new Player(PlayerID.PLAYER_1, Sq.constant(new Player.LifeState(0, State.INVULNERABLE)), null, 1, 2);
+    }
+    @Test (expected=IllegalArgumentException.class)
+    public void consrtuctionWithNegativePositionReturnError(){
+        Player p = new Player(PlayerID.PLAYER_1, Sq.constant(new Player.LifeState(0, State.INVULNERABLE)), Sq.constant(new Player.DirectedPosition(new SubCell(-1,-4), Direction.N)), -1, 2);
+    }
+    @Test (expected=NullPointerException.class)
+    public void consrtuctionWithNullIDReturnError(){
+        Player p = new Player(null, Sq.constant(new Player.LifeState(0, State.INVULNERABLE)), Sq.constant(new Player.DirectedPosition(new SubCell(-1,-4), Direction.N)), -1, 2);
+    }
+    
+    @Test
+    public void 
 }
