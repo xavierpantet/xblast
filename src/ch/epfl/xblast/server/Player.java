a9715387bee1b628374170f1bca7f6582a6b20d8
Xavier Pierre-Alain Pantet <xavier.pantet@epfl.ch>
2016-03-11 12:18:15 +0100

    Player terminée non commentée

--- src/ch/epfl/xblast/server/Player.java
+++ src/ch/epfl/xblast/server/Player.java
@@ -4,9 +4,11 @@ import java.util.Objects;
 
 import ch.epfl.cs108.Sq;
 import ch.epfl.xblast.ArgumentChecker;
+import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.Direction;
 import ch.epfl.xblast.PlayerID;
 import ch.epfl.xblast.SubCell;
+import ch.epfl.xblast.server.Player.LifeState.State;
 
 /**
  * Classe représentant un joueur
@@ -14,19 +16,108 @@ import ch.epfl.xblast.SubCell;
  */
 public final class Player {
     private final PlayerID id;
-    private final Sq<LifeState> lifeState;
+    private final Sq<LifeState> lifeStates;
     private final Sq<DirectedPosition> directedPositions;
     private final int maxBombs;
     private final int bombRange;
     
     public Player(PlayerID id, Sq<LifeState> lifeStates, Sq<DirectedPosition> directedPos, int maxBombs, int bombRange) throws NullPointerException, IllegalArgumentException{
         this.id=Objects.requireNonNull(id);
-        this.lifeState=Objects.requireNonNull(lifeStates);
+        this.lifeStates=Objects.requireNonNull(lifeStates);
         this.directedPositions=Objects.requireNonNull(directedPos);
         this.maxBombs=ArgumentChecker.requireNonNegative(maxBombs);
         this.bombRange=ArgumentChecker.requireNonNegative(bombRange);
     }
     
+    public Player(PlayerID id, int lives, Cell position, int maxBombs, int bombRange) throws NullPointerException, IllegalArgumentException{
+        this(id, createStateSequence(lives),
+                Sq.constant(new DirectedPosition(SubCell.centralSubCellOf(position), Direction.S)),
+                maxBombs,
+                bombRange);
+    }
+    
+    public PlayerID id(){
+        return id;
+    }
+    
+    public Sq<LifeState> lifeStates(){
+        return lifeStates;
+    }
+    
+    public LifeState lifeState(){
+        return lifeStates.head();
+    }
+    
+    public Sq<LifeState> statesForNextLife(){
+        int nbLives=lives();
+        Sq<LifeState> toReturn = Sq.constant(new LifeState(nbLives, State.DYING)).limit(Ticks.PLAYER_DYING_TICKS);
+        if(nbLives==0){
+            toReturn.concat(Sq.constant(new LifeState(nbLives, State.DEAD)));
+        }
+        else{
+            toReturn.concat(Sq.constant(new LifeState(nbLives, State.INVULNERABLE)).limit(Ticks.PLAYER_INVULNERABLE_TICKS));
+            toReturn.concat(Sq.constant(new LifeState(nbLives-1, State.VULNERABLE)));
+        }
+        return toReturn;
+        
+    }
+    
+    public int lives(){
+        return lifeStates.head().lives();
+    }
+    
+    public boolean isAlive(){
+        return (lives()>0)? true:false;
+    }
+    
+    public Sq<DirectedPosition> directedPositions(){
+        return directedPositions;
+    }
+    
+    public SubCell position(){
+        return directedPositions.head().position();
+    }
+    
+    public Direction direction(){
+        return directedPositions.head().direction();
+    }
+    
+    public int maxBombs(){
+        return maxBombs;
+    }
+    
+    public Player withMaxBombs(int newMaxBombs){
+        return new Player(id, lifeStates, directedPositions, newMaxBombs, bombRange);
+    }
+    
+    public int bombRange(){
+        return bombRange;
+    }
+    
+    public Player withBombRange(int newBombRange){
+        return new Player(id, lifeStates, directedPositions, maxBombs, newBombRange);
+    }
+    
+    public Bomb newBomb(){
+        return new Bomb(id, position().containingCell(), Ticks.BOMB_FUSE_TICKS, bombRange);
+    }
+    
+    private static Sq<LifeState> createStateSequence(int lives) throws IllegalArgumentException{
+        if(lives==0){
+            return Sq.constant(new LifeState(lives, State.DEAD));
+        }
+        else if(lives>0){
+            Sq<LifeState> lifeState = Sq.constant(new LifeState(lives, State.INVULNERABLE)).limit(Ticks.PLAYER_INVULNERABLE_TICKS);
+            lifeState.concat(Sq.constant(new LifeState(lives, State.VULNERABLE)));
+            return lifeState;
+        }
+        else{throw new IllegalArgumentException("Le nombre de vie doit être non négatif");}
+    }
+    
+    /**
+     * Classe représentant une position dirigée du joueur (sa position et la direction dans laquelle il regarde)
+     * @author Timothée Duran (258683)
+     */
     public static final class DirectedPosition {
 
         private final SubCell position;
@@ -115,6 +206,10 @@ public final class Player {
         }
     }
     
+    /**
+     * Classe représentant l'état de vie d'un joueur (nombre de vies et état)
+     * @author Timothée Duran (258683)
+     */
     public static final class LifeState {
         
         private final int lives;
