b6945dd8486c5835cf288ef0a5ce4ec104252b5f
Xavier Pierre-Alain Pantet <xavier.pantet@epfl.ch>
2016-03-13 12:16:40 +0100

    Méthode permutations de Lists avec tests

--- XBlastTest/ch/epfl/xblast/ListsTest.java
+++ XBlastTest/ch/epfl/xblast/ListsTest.java
@@ -3,6 +3,7 @@ package ch.epfl.xblast;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -56,7 +57,31 @@ public class ListsTest {
         List<Integer> l = new ArrayList<>();
         List<Integer> t = new ArrayList<>();
         List<Integer> mirror = Lists.mirrored(l);
-        //assertEquals(t, mirror);
+        assertEquals(t, mirror);
+    }
+    
+    @Test
+    public void permutationsWorks(){
+        List<Integer> l = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        List<List<Integer>> check = new ArrayList<>();
+        int factorial=1;
+        long startTime = System.currentTimeMillis();
+        List<List<Integer>> t = Lists.permutations(l);
+        long endTime   = System.currentTimeMillis();
+        System.out.println("Temps de calcul: " + (endTime-startTime) + " ms");
+        
+        for(int i=2; i<=l.size(); i++){
+            factorial*=i;
+        }
+        
+        for(List<Integer> e:t){
+            //System.out.println(e);
+            if(!check.contains(e)){
+                check.add(e);
+            }
+        }
+        if(!check.contains(l)){check.add(l);}
+        assertEquals(check.size(), factorial);
     }
 
 }
--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -1,6 +1,7 @@
 package ch.epfl.xblast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,5 +27,60 @@ public final class Lists {
             list.addAll(sub);
             return list;
         }else{throw new IllegalArgumentException("La liste ne peut pas être vide");}
+    }
+    
+    /**
+     * Calcule de manière récursive l'ensemble des combinaisons possibles sur un tableau
+     * @param l
+     * @return une liste des combinaisons de la liste donnée
+     */
+    public static <T> List<List<T>> permutations(List<T> l){
+        int size = l.size();
+        
+        // Si le tableau est vide ou qu'il contient 1 élément
+        if(size==0 || size==1){
+            // On retourne un tableau de tableau vide, ou un tableau contenant un tableau du premier élément
+            List<List<T>> trivialArray = (size==0)? new ArrayList<>(Arrays.asList(Arrays.asList())) : new ArrayList<>(Arrays.asList(Arrays.asList(l.get(0))));
+            return trivialArray;
+        }
+        else{
+            T firstElement = l.get(0);
+            
+            // Cas d'arrêt de la récursion
+            // Permutations d'un tableau [X, Y] --> [[X, Y], [Y, X]]
+            if(size==2){
+                List<List<T>> queue = new ArrayList<>();
+                queue.add(new ArrayList<>(l));
+                Collections.reverse(l);
+                queue.add(new ArrayList<>(l));
+                return queue;
+            }
+            
+            // Si le tableau contient plus de deux éléments
+            else{
+                
+                // On applique la récursion sur la queue du tableau
+                List<List<T>> recursive = new ArrayList<>();
+                recursive.addAll(permutations(l.subList(1, size)));
+                
+                List<T> temp;
+                List<List<T>> headPermut=new ArrayList<>();
+                int elementSize=0;
+                
+                // Pour chaque élément de recursive
+                for(List<T> element:recursive){
+                    elementSize=element.size();
+                    
+                    // On ajoute la tête à toutes les places possibles
+                    for(int i=0; i<=elementSize; i++){
+                        temp=new ArrayList<>(element);
+                        temp.add(i, firstElement);
+                        headPermut.add(temp);
+                    }
+
+                }
+                return headPermut;
+            }
+        }
     }
 }
