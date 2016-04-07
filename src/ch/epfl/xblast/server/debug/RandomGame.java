9bd2607ac6a620ac5a06facc7be02ed48239410f
Timothée Duran <timothee.duran@epfl.ch>
2016-04-07 19:20:31 +0200

    Modification du print

--- src/ch/epfl/xblast/server/debug/RandomGame.java
+++ src/ch/epfl/xblast/server/debug/RandomGame.java
@@ -40,12 +40,9 @@ public class RandomGame {
             g=g.next(randomShit.randomSpeedChangeEvents(), randomShit.randomBombDropEvents());
             GameStatePrinter.printGameState(g);
            
-               
+                Thread.sleep(50);
             
             System.out.println("\u001b[2J");
-        }for (int i=0; i<60; i++){
-            g=g.next(randomShit.randomSpeedChangeEvents(), randomShit.randomBombDropEvents());
-            GameStatePrinter.printGameState(g); 
         }
     }
 
