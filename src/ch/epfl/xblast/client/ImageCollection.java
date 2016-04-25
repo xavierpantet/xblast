0ae4fd405043a18a1bd317f824540136a49fd8ed
Xavier Pierre-Alain Pantet <xavier.pantet@epfl.ch>
2016-04-25 13:07:07 +0200

    Ajout package + ImageCollection

---
+++ src/ch/epfl/xblast/client/ImageCollection.java
@@ -0,0 +1,38 @@
+package ch.epfl.xblast.client;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+
+import javax.imageio.ImageIO;
+
+public final class ImageCollection {
+    private final String collection;
+    public ImageCollection(String dir){
+        this.collection=dir;
+    }
+    
+    public Image image(int i) throws NoSuchElementException, URISyntaxException, IOException{
+        Image im = imageOrNull(i);
+        if(im!=null){
+            return im;
+        }
+        else{throw new NoSuchElementException("Aucune image trouvé pour l'index " + i);}
+    }
+    
+    public Image imageOrNull(int i) throws URISyntaxException, IOException{
+        File dir = new File(ImageCollection.class
+                .getClassLoader()
+                .getResource(collection)
+                .toURI());
+        File[] files = dir.listFiles();
+        for(File f:files){
+            if(i==Integer.parseInt(f.getName())){
+                return ImageIO.read(f);
+            }
+        }
+        return null;
+    }
+}
---
+++ test/ch/epfl/xblast/client/ImageCollectionTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.xblast.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImageCollectionTest {
+
+    @Test
+    public void imageCollection() {
+        ImageCollection col = new ImageCollection("");
+    }
+
+}
