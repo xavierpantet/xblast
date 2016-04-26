ddb9fef099050974114592ed770a7cb395b3ce74
Xavier Pierre-Alain Pantet <xavier.pantet@epfl.ch>
2016-04-26 13:37:42 +0200

    ImageCollection commentée

--- src/ch/epfl/xblast/client/ImageCollection.java
+++ src/ch/epfl/xblast/client/ImageCollection.java
@@ -8,12 +8,30 @@ import java.util.NoSuchElementException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Classe qui permet d'accéder à un élément d'un répertoire en connaissant son index
+ * @author Xavier Pantet (260473)
+ */
 public final class ImageCollection {
     private final String collection;
+    
+    /**
+     * Constructeur qui permet de construire une collection d'accès aux images en connaissant son nom
+     * @param dir   le répertoire de la collection
+     */
     public ImageCollection(String dir){
         this.collection=dir;
     }
     
+    /**
+     * Retourne une image dans la collection d'après son index ou lève NoSuchElementException
+     * si l'index ne correspond à aucune image
+     * @param i l'index de l'image
+     * @return l'image pour l'index i dans la collection
+     * @throws NoSuchElementException
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public Image image(int i) throws NoSuchElementException, URISyntaxException, IOException{
         Image im = imageOrNull(i);
         if(im!=null){
@@ -22,6 +40,14 @@ public final class ImageCollection {
         else{throw new NoSuchElementException("Aucune image trouvé pour l'index " + i);}
     }
     
+    /**
+     * Retourne une image dans la collection d'après son index ou null
+     * si l'index ne correspond à aucune image
+     * @param i l'index de l'image
+     * @return l'image pour l'index i dans la collection
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public Image imageOrNull(int i) throws URISyntaxException, IOException{
         File dir = new File(ImageCollection.class
                 .getClassLoader()
@@ -29,12 +55,9 @@ public final class ImageCollection {
                 .toURI());
         File[] files = dir.listFiles();
         for(File f:files){
-            //try{
-                if(i==Integer.parseInt(f.getName().substring(0, 3))){
-                    return ImageIO.read(f);
-                }
-            //}
-            //catch(Exception e){}
+            if(i==Integer.parseInt(f.getName().substring(0, 3))){
+                return ImageIO.read(f);
+            }      
         }
         return null;
     }
